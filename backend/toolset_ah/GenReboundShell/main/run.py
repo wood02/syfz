94d2595e7deefe261c8e788e083d5620fa14929d
W_OOD <jiaaa213@163.com>
2022-06-01 11:06:26 +0800

    修改toolset

--- backend/toolset_ah/GenReboundShell/main/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding:utf-8
-from loguru import logger
-
-
-async def hello_world(name):
-    logger.info("[Hello World] APP 执行参数为: {name}", name=name)
-    return {"status": 0, "result": "Hello," + name}
