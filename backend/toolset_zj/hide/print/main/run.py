f164d386d271fa1278aec33efe97e37336abbb45
W_OOD <jiaaa213@163.com>
2022-06-01 11:00:54 +0800

    修改toolset

--- backend/toolset_zj/hide/print/main/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding:utf-8
-from loguru import logger
-
-
-async def print_content(content):
-    logger.info("[Print] APP 执行参数为: {content}", content=content)
-    return {"status": 0, "result": content}
